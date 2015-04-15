be6d1264467354c2462098c8c62761714f99ca94
muit <miguel_3c@hotmail.com>
2015-04-15 21:40:35 +0200

    Main Menu created

--- Assets/Source/Core/MenuController.cs
+++ Assets/Source/Core/MenuController.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class MenuController : MonoBehaviour {
 	public string name;
 
 	public GameObject login;
 
-	// Use this for initialization
-	void Start () {
-		if(login == null) login = GameObject.Find("Canvas/Login");
+    public Canvas mainMenu;
+    public List<Canvas> subMenus = new List<Canvas>();
+
+
+    // Use this for initialization
+    void Start () {
+		if(!login) login = GameObject.Find("Canvas/Login");
 	}
 	
 	// Update is called once per frame
@@ -26,4 +30,33 @@ public class MenuController : MonoBehaviour {
 		GameObject nameInput = GameObject.Find("NameInputValue");
 		//Login(nameInput.GetComponent<UIInput>().text);
 	}
+
+    public void ShowMainMenu()
+    {
+        HideSubMenus();
+        mainMenu.gameObject.SetActive(true);
+    }
+
+    public void ShowSubMenu(int id)
+    {
+        mainMenu.gameObject.SetActive(false);
+        Canvas subMenu = subMenus[id];
+        if (subMenu)
+            subMenu.gameObject.SetActive(true);
+    }
+
+    public void HideSubMenus() {
+        foreach (Canvas subMenu in subMenus)
+            subMenu.gameObject.SetActive(false);
+    }
+
+
+    public void LoadScene(int id)
+    {
+        Application.LoadLevel(id);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
 }
